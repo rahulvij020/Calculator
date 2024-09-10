a04e02a34ef54c098f2b3e13cef7ad094549e073
rahulvij020 <vijrahul020@gmail.com>
2024-09-10 13:43:15 +0530

    Simple Calculator

---
+++ calculator.js
@@ -0,0 +1,16 @@
+const display=document.querySelector("#display");
+
+function appendToDisplay(input){
+    display.value+=input;
+}
+function calculate(){
+    try{
+    display.value=eval(display.value);
+    }
+    catch(error){
+        display.value="Error";
+    }
+}
+function clearDisplay(){
+    display.value="";
+}
